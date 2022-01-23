36649aa575593e789df1d3ea03105abbd1f41703
tom-osborne <tom.j.osborne@gmail.com>
2022-01-23 00:29:11 +0000

    setup dashboard template

--- app.py
+++ app.py
@@ -23,5 +23,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
